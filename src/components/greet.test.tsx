ad898a49ee5a72fdd3e500fc0d64dbfa1e14935e
Gunjannegi <gunjan.negi.5249@gmail.com>
2026-02-24 16:58:03 +0530

    Created test for Greet file

---
+++ src/components/greet.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react"
+import Greet from "./greet"
+
+/**
+ * Greet returns the text hello and if a name is passed into the component
+ * It should render hello followed by the name
+ */
+test('Greet renders correctly', ()=>{
+    render(<Greet/>);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+});
+
+test('Greet renders with a name',()=>{
+    render(<Greet name='Gunjan'/>);
+    const textElement = screen.getByText('Hello Gunjan');
+    expect(textElement).toBeInTheDocument();
+})
