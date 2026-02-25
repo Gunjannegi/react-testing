661929e185d346ebea5ff786071de74c922ec9ae
Gunjannegi <gunjan.negi.5249@gmail.com>
2026-02-25 20:57:27 +0530

    file conventions

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react"
-import Greet from "./greet"
+import Greet from "./greet";
 
 /**
  * Greet returns the text hello and if a name is passed into the component
  * It should render hello followed by the name
  */
+
+describe('Greet',()=>{
 test('Greet renders correctly', ()=>{
     render(<Greet/>);
     const textElement = screen.getByText(/hello/i);
@@ -15,4 +17,5 @@ test('Greet renders with a name',()=>{
     render(<Greet name='Gunjan'/>);
     const textElement = screen.getByText('Hello Gunjan');
     expect(textElement).toBeInTheDocument();
+})
 })
