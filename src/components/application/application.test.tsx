9a129341d90d1af36b2340e50812554498cb2380
Gunjannegi <gunjan.negi.5249@gmail.com>
2026-03-06 17:23:29 +0530

    getByRole and getByLabelText query

---
+++ src/components/application/application.test.tsx
@@ -0,0 +1,36 @@
+import { render,screen } from "@testing-library/react"
+import { Application } from "./application"
+
+describe('Application',()=>{
+    test('renders correctly',()=>{
+        render(<Application/>);
+        // const pageHeading = screen.getByRole('heading',{name:'Job application form'});
+        const pageHeading = screen.getByRole('heading',{level:1});
+        expect(pageHeading).toBeInTheDocument();
+
+        // const sectionHeading = screen.getByRole('heading',{name:'Section 1'});
+        const sectionHeading = screen.getByRole('heading',{level:2});
+        expect(sectionHeading).toBeInTheDocument();
+
+        const nameElement = screen.getByRole('textbox',{name:'Name'});
+        expect(nameElement).toBeInTheDocument();
+
+        const nameElement2 = screen.getByLabelText('Name',{selector:'input'});
+        expect(nameElement2).toBeInTheDocument();
+
+        const bioElement = screen.getByRole('textbox',{name:'Bio'});
+        expect(bioElement).toBeInTheDocument();
+
+        const jobLocationElement = screen.getByRole('combobox');
+        expect(jobLocationElement).toBeInTheDocument();
+
+        const termsElement = screen.getByRole('checkbox');
+        expect(termsElement).toBeInTheDocument();
+
+        const termsElement2 = screen.getByLabelText('I agree to the terms and conditions');
+        expect(termsElement2).toBeInTheDocument();
+
+        const submitButton = screen.getByRole('button');
+        expect(submitButton).toBeInTheDocument();
+    })
+})
